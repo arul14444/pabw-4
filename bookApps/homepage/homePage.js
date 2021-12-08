df1ffcffa9e8bdb51d958a001f1de0aed754751d
arul14444 <95666359+arul14444@users.noreply.github.com>
2021-12-08 21:27:07 +0700

    Add files via upload

---
+++ bookApps/homepage/homePage.js
@@ -0,0 +1,9 @@
+var user = 'Budi'
+var masuk =1000000
+var keluar= 200000
+var untung= masuk-keluar
+
+document.getElementById('user').innerHTML = user
+document.getElementById('jumlahPemasukan').innerHTML = masuk
+document.getElementById('jumlahPengeluaran').innerHTML = keluar
+document.getElementById('keuntungan').innerHTML = untung
